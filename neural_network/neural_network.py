6134bc6f7bc697c8cb143f2971cf9e7a6f27c8ba
magnushelliesen <magnus.helliesen@gmail.com>
2023-11-24 07:10:39 +0100

    Random inital weights and biases

--- neural_network/neural_network.py
+++ neural_network/neural_network.py
@@ -30,17 +30,17 @@ class NeuralNetwork():
         self._biases = []
 
         # Setup weights and biases from input layer to first hidden layer
-        self._weights += np.zeros((dim_hidden, dim_input)),
-        self._biases += np.zeros(dim_hidden),
+        self._weights += np.random.normal(size=(dim_hidden, dim_input))/self.dim_input,
+        self._biases += np.random.normal(size=dim_hidden)/self.dim_input,
 
         # Setup weights and biases between hidden layers
         for i in range(self.n_hidden-1):
-            self._weights += np.zeros((dim_hidden, dim_hidden)),
-            self._biases += np.zeros(dim_hidden),
+            self._weights += np.random.normal(size=(dim_hidden, dim_hidden))/self.dim_hidden,
+            self._biases += np.random.normal(size=dim_hidden)/self.dim_hidden,
 
         # Setup weights and biases from last hidden layer to output layer
-        self._weights += np.zeros((dim_output, dim_hidden)),
-        self._biases += np.zeros(dim_output),
+        self._weights += np.random.normal(size=(dim_output, dim_hidden))/self.dim_hidden,
+        self._biases += np.random.normal(size=dim_output)/self.dim_hidden,
 
     @property
     def dim_input(self):
