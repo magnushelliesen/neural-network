ae04ac13255728f4db5a22b1edb15c6e078a1416
magnushelliesen <magnus.helliesen@gmail.com>
2023-12-13 06:49:49 +0100

    Small change to make stuff smoother

--- user_input.py
+++ user_input.py
@@ -9,12 +9,10 @@ lines = draw_input()
 X = convert_to_bitmap(lines)
 x = x = matrix_mapper(X, 28, 28)
 x[x<200] = 0
-plt.imshow(x, cmap='gray')
-plt.show()
 
 digit = (255-x).reshape(784)
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 2.5))
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
 ax1.bar(height=nn.predict(digit), x=[f'{i}' for i in range(10)], color='b')
 ax2.imshow((255-digit).reshape((28, 28)), cmap='gray')
 ax2.set_axis_off()
