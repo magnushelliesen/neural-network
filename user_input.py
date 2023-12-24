7a74c2e70b9997dd9c79467adf49f85c282f7db7
magnushelliesen <magnus.helliesen@gmail.com>
2023-12-24 09:59:32 +0100

    Learn as we go :)

--- user_input.py
+++ user_input.py
@@ -1,3 +1,8 @@
+"""
+This is a bit messy, sorry about that. But it works :)
+"""
+
+import numpy as np
 import matplotlib.pyplot as plt
 from input_interpreter.digit_input import draw_input, convert_to_bitmap, matrix_mapper
 import pickle
@@ -17,9 +22,19 @@ x = matrix_mapper(X, 28, 28)
 x[x<200] = 0
 digit = (255-x).reshape(784)
 
+digit_norm = (digit-digit.mean())/digit.std()
+
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
-ax1.bar(height=nn.predict(digit), x=[f'{i}' for i in range(10)], color='b')
+ax1.bar(height=nn.predict(digit_norm), x=[f'{i}' for i in range(10)], color='b')
 ax2.imshow((255-digit).reshape((28, 28)), cmap='gray')
 ax2.set_axis_off()
 plt.show()
 		
+correct = input('What was the digit?: ')
+
+print('Training before: ', nn.training)
+nn.train([[digit_norm, 1*np.array([int(correct) == i for i in range(10)])]], 1, 0.001)
+print('Training after: ', nn.training)
+
+with open('nn.pickle', 'wb') as f:
+    pickle.dump(nn, f)
