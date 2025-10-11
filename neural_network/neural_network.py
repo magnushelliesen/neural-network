65e17a4a693b8c3b8f1d679fb84e3dabe1758a89
magnushelliesen <magnus.helliesen@gmail.com>
2025-10-11 18:54:15 +0200

    Black linting

--- neural_network/neural_network.py
+++ neural_network/neural_network.py
@@ -9,18 +9,14 @@ from typing import Union, List, Tuple, Iterator, Any
 from numpy.typing import NDArray
 
 
-class NeuralNetwork():
+class NeuralNetwork:
     """
     A simple neural network class with multiple hidden layers.
     """
 
     def __init__(
-            self,
-            dim_input: int,
-            dim_hidden: int,
-            n_hidden: int,
-            dim_output: int
-            ) -> None:
+        self, dim_input: int, dim_hidden: int, n_hidden: int, dim_output: int
+    ) -> None:
         """
         Initializes the neural network with given dimensions for input, hidden, and output layers.
         Randomly initializes weights and biases for each layer.
@@ -45,24 +41,30 @@ class NeuralNetwork():
         self._dim_output = dim_output
 
         self._last_input: NDArray[np.float64] | None = None
-        self._last_activations: Tuple[NDArray[np.float64] , ...] | None = None
+        self._last_activations: Tuple[NDArray[np.float64], ...] | None = None
 
         # Setup weights and biases
         self._weights: Tuple[NDArray[np.float64], ...] = tuple()
         self._biases: Tuple[NDArray[np.float64], ...] = tuple()
 
         # Setup weights and biases from input layer to first hidden layer
-        self._weights += (np.random.rand(dim_hidden, dim_input)-0.5)/self.dim_hidden,
-        self._biases += (np.random.rand(dim_hidden)-0.5)/self.dim_hidden,
+        self._weights += (
+            (np.random.rand(dim_hidden, dim_input) - 0.5) / self.dim_hidden,
+        )
+        self._biases += ((np.random.rand(dim_hidden) - 0.5) / self.dim_hidden,)
 
         # Setup weights and biases between hidden layers
-        for i in range(self.n_hidden-1):
-            self._weights += (np.random.rand(dim_hidden, dim_hidden)-0.5)/self.dim_hidden,
-            self._biases += (np.random.rand(dim_hidden)-0.5)/self.dim_hidden,
+        for i in range(self.n_hidden - 1):
+            self._weights += (
+                (np.random.rand(dim_hidden, dim_hidden) - 0.5) / self.dim_hidden,
+            )
+            self._biases += ((np.random.rand(dim_hidden) - 0.5) / self.dim_hidden,)
 
         # Setup weights and biases from last hidden layer to output layer
-        self._weights += (np.random.rand(dim_output, dim_hidden)-0.5)/self.dim_output,
-        self._biases += (np.random.rand(dim_output)-0.5)/self.dim_output,
+        self._weights += (
+            (np.random.rand(dim_output, dim_hidden) - 0.5) / self.dim_output,
+        )
+        self._biases += ((np.random.rand(dim_output) - 0.5) / self.dim_output,)
 
         # Storing initial weigths and biases
         self._weights0 = tuple(x.copy() for x in self.weights)
@@ -106,11 +108,11 @@ class NeuralNetwork():
 
     @property
     def delta_weights(self) -> Tuple[NDArray[np.float64], ...]:
-        return tuple(x-y for x, y in zip(self.weights, self.weights0))
+        return tuple(x - y for x, y in zip(self.weights, self.weights0))
 
     @property
     def delta_biases(self) -> Tuple[NDArray[np.float64], ...]:
-        return tuple(x-y for x, y in zip(self.biases, self.biases0))
+        return tuple(x - y for x, y in zip(self.biases, self.biases0))
 
     @property
     def last_input(self) -> NDArray[np.float64] | None:
@@ -121,7 +123,7 @@ class NeuralNetwork():
         return self._last_activations
 
     def __repr__(self) -> str:
-        return f'NeuralNetwork({self.dim_input}, {self.dim_hidden}, {self.n_hidden}, {self.dim_output})'
+        return f"NeuralNetwork({self.dim_input}, {self.dim_hidden}, {self.n_hidden}, {self.dim_output})"
 
     @staticmethod
     def _sigmoid(x: NDArray[np.float64]) -> NDArray[np.float64]:
@@ -139,7 +141,7 @@ class NeuralNetwork():
             Output after applying the sigmoid function element-wise.
         """
 
-        return 1/(1+np.exp(-x))
+        return 1 / (1 + np.exp(-x))
 
     @staticmethod
     def _relu(x: NDArray[np.float64]) -> NDArray[np.float64]:
@@ -175,9 +177,11 @@ class NeuralNetwork():
             Output after applying the softmax function.
         """
 
-        return np.exp(x)/np.exp(x).sum()
+        return np.exp(x) / np.exp(x).sum()
 
-    def _activations(self, input: NDArray[np.float64]) -> Tuple[NDArray[np.float64], ...]:
+    def _activations(
+        self, input: NDArray[np.float64]
+    ) -> Tuple[NDArray[np.float64], ...]:
         """
         Calculates activations through the network layers.
 
@@ -193,9 +197,11 @@ class NeuralNetwork():
         """
 
         if input.shape[0] != self.dim_input:
-            raise IndexError(f'Input must have {self.dim_input} rows, not {input.shape[0]}')
+            raise IndexError(
+                f"Input must have {self.dim_input} rows, not {input.shape[0]}"
+            )
         if len(input.shape) != 1:
-            raise IndexError('Input must be 1d array')
+            raise IndexError("Input must be 1d array")
 
         x = input
         activations: Tuple[NDArray[np.float64], ...] = tuple()
@@ -203,16 +209,16 @@ class NeuralNetwork():
         # Forwardpropagation
         for i, (weights, biases) in enumerate(zip(self.weights, self.biases)):
             if i == self.n_hidden:
-                x = self._softmax(weights.dot(x)+biases)
+                x = self._softmax(weights.dot(x) + biases)
             elif i == 0:
-                x = self._relu(weights.dot(x)+biases)
+                x = self._relu(weights.dot(x) + biases)
             else:
-                x = self._sigmoid(weights.dot(x)+biases)
+                x = self._sigmoid(weights.dot(x) + biases)
 
             if all(np.isfinite(x)) is False:
-                raise ValueError('Weights and biases give np.nan or np.inf')
+                raise ValueError("Weights and biases give np.nan or np.inf")
 
-            activations += x,
+            activations += (x,)
 
         self._last_input = input
         self._last_activations = activations
@@ -220,8 +226,8 @@ class NeuralNetwork():
         return activations
 
     def predict(
-            self, input: np.ndarray[Tuple[int], np.dtype[np.float64]]
-            ) -> np.ndarray[Tuple[int], np.dtype[np.float64]]:
+        self, input: np.ndarray[Tuple[int], np.dtype[np.float64]]
+    ) -> np.ndarray[Tuple[int], np.dtype[np.float64]]:
         """
         Predicts the output for a given input using the neural network.
 
@@ -239,14 +245,14 @@ class NeuralNetwork():
         return self._activations(input)[-1]
 
     def train(
-            self,
-            data: Union[
-                Tuple[Tuple[NDArray[np.float64], NDArray[np.float64]]],
-                Tuple[Tuple[NDArray[np.float64], NDArray[np.float64]]]
-                ],
-            n: int,
-            step: float=0.1
-            ) -> None:
+        self,
+        data: Union[
+            Tuple[Tuple[NDArray[np.float64], NDArray[np.float64]]],
+            Tuple[Tuple[NDArray[np.float64], NDArray[np.float64]]],
+        ],
+        n: int,
+        step: float = 0.1,
+    ) -> None:
         """
         Trains the neural network using backpropagation.
 
@@ -264,9 +270,10 @@ class NeuralNetwork():
             random_data = choices(data, k=n)
         elif isinstance(data, pd.DataFrame):
             random_df = data.sample(n=n, replace=True)
-            raise RuntimeError('No support for dataframe yet')
+            raise RuntimeError("No support for dataframe yet")
 
-        weights, biases = tuple(x.copy() for x in self.weights), tuple(x.copy() for x in self.biases)
+        weights = tuple(x.copy() for x in self.weights)
+        biases = tuple(x.copy() for x in self.biases)
 
         for input, target in random_data:
             try:
@@ -279,21 +286,21 @@ class NeuralNetwork():
                     bias += delta_bias
             except ValueError:
                 self._weights, self._biases = weights, biases
-                print('Try reducing learning rate')
+                print("Try reducing learning rate")
                 return
 
         self._training += n
 
     def batch_train(
-            self,
-            data: Union[
-                Tuple[Tuple[NDArray[np.float64], NDArray[np.float64]]],
-                Tuple[Tuple[NDArray[np.float64], NDArray[np.float64]]]
-                ],
-            n: int,
-            batch_size: int=10,
-            step: float=0.1
-            ) -> None:
+        self,
+        data: Union[
+            Tuple[Tuple[NDArray[np.float64], NDArray[np.float64]]],
+            Tuple[Tuple[NDArray[np.float64], NDArray[np.float64]]],
+        ],
+        n: int,
+        batch_size: int = 10,
+        step: float = 0.1,
+    ) -> None:
         """
         Trains the neural network using backpropagation.
 
@@ -311,9 +318,10 @@ class NeuralNetwork():
             random_data_batches = self.batchify(choices(data, k=n), batch_size)
         elif isinstance(data, pd.DataFrame):
             random_df = data.sample(n=n, replace=True)
-            raise RuntimeError('No support for dataframe yet')
+            raise RuntimeError("No support for dataframe yet")
 
-        weights, biases = tuple(x.copy() for x in self.weights), tuple(x.copy() for x in self.biases)
+        weights = tuple(x.copy() for x in self.weights)
+        biases = tuple(x.copy() for x in self.biases)
 
         for random_data_batch in list(random_data_batches):
 
@@ -323,17 +331,23 @@ class NeuralNetwork():
             for random_data in random_data_batch:
                 input, target = random_data
                 try:
-                    delta_weights_update, delta_biases_update = self.backpropagation(input, target, step)
+                    delta_weights_update, delta_biases_update = self.backpropagation(
+                        input, target, step
+                    )
 
-                    for delta_weight, delta_weight_update in zip(delta_weights, delta_weights_update):
+                    for delta_weight, delta_weight_update in zip(
+                        delta_weights, delta_weights_update
+                    ):
                         delta_weight += delta_weight_update
 
-                    for delta_biase, delta_biase_update in zip(delta_biases, delta_biases_update):
+                    for delta_biase, delta_biase_update in zip(
+                        delta_biases, delta_biases_update
+                    ):
                         delta_biase += delta_biase_update
 
                 except ValueError:
                     self._weights, self._biases = weights, biases
-                    print('Try reducing learning rate')
+                    print("Try reducing learning rate")
                     return
 
             for weight, delta_weight in zip(self._weights, delta_weights):
@@ -345,11 +359,8 @@ class NeuralNetwork():
         self._training += n
 
     def backpropagation(
-            self,
-            input: NDArray[np.float64],
-            target: NDArray[np.float64],
-            step: float=0.1
-            ) -> Tuple[List[NDArray[np.float64]], List[NDArray[np.float64]]]:
+        self, input: NDArray[np.float64], target: NDArray[np.float64], step: float = 0.1
+    ) -> Tuple[List[NDArray[np.float64]], List[NDArray[np.float64]]]:
         """
         Performs backpropagation to update weights and biases based on the input and target output.
 
@@ -362,27 +373,31 @@ class NeuralNetwork():
         step : float, optional
             Learning rate (default is 0.1).
         """
-        
+
         activations = self._activations(input)
         delta_weights = [np.zeros_like(x) for x in self.weights]
         delta_biases = [np.zeros_like(x) for x in self.biases]
 
         # Backpropagation
-        for i in reversed(range(self.n_hidden+1)):
+        for i in reversed(range(self.n_hidden + 1)):
             if i == self.n_hidden:
-                delta = activations[i]-target
-                delta_weights[i] -= step*np.outer(delta, activations[i-1])
+                delta = activations[i] - target
+                delta_weights[i] -= step * np.outer(delta, activations[i - 1])
             elif i == 0:
-                delta = delta.dot(self.weights[i+1]).T*(activations[i]>0)
-                delta_weights[i] -= step*np.outer(delta, input)
+                delta = delta.dot(self.weights[i + 1]).T * (activations[i] > 0)
+                delta_weights[i] -= step * np.outer(delta, input)
             else:
-                delta = delta.dot(self.weights[i+1]).T*activations[i]*(1-activations[i])
-                delta_weights[i] -= step*np.outer(delta, activations[i-1])
-            delta_biases[i] -= step*delta
+                delta = (
+                    delta.dot(self.weights[i + 1]).T
+                    * activations[i]
+                    * (1 - activations[i])
+                )
+                delta_weights[i] -= step * np.outer(delta, activations[i - 1])
+            delta_biases[i] -= step * delta
 
         return delta_weights, delta_biases
 
     @staticmethod
     def batchify(x: List[Any], n: int) -> Iterator[List[Any]]:
         for i in range(0, len(x), n):
-            yield x[i:i+n]
+            yield x[i : i + n]
